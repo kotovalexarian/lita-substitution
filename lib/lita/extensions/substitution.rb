d6e6229f2395a6d578cd88fadac6e07b50fa1001
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2015-10-24 22:14:50 +0000

    Implement extension

--- lib/lita/extensions/substitution.rb
+++ lib/lita/extensions/substitution.rb
@@ -10,6 +10,14 @@ module Lita
     # Shell-like command substitution for the Lita chat bot.
     #
     class Substitution
+      def self.call(payload)
+        message = payload[:message]
+        route = payload[:route]
+
+        !(message.body =~ /[^\$]\$\(/) || route.extensions[:substitution]
+      end
+
+      Lita.register_hook(:validate_route, self)
     end
   end
 end
