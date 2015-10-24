3f669b9c6ebc3cb632e876005902ba8600937cc5
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2015-10-24 22:51:19 +0000

    Parser stub

--- lib/lita/handlers/substitution.rb
+++ lib/lita/handlers/substitution.rb
@@ -10,7 +10,18 @@ module Lita
     # Shell-like command substitution for the Lita chat bot.
     #
     class Substitution < Handler
-      # insert handler code here
+      route(/[^\$]\$\(/, command: true, substitution: true) do |response|
+        command, substitutions = parse(response.message.body)
+
+        response.reply(command.inspect)
+        response.reply(substitutions.inspect)
+      end
+
+    protected
+
+      def parse(text)
+        ['randcase ', { 9 => 'fake lorem sentence' }]
+      end
 
       Lita.register_handler(self)
     end
