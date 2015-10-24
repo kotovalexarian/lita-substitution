a3c1c0a459427e01cc892a9ae19dbb385142da79
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2015-10-24 23:45:57 +0000

    Implement subcommands and substitution

--- lib/lita/handlers/substitution.rb
+++ lib/lita/handlers/substitution.rb
@@ -13,11 +13,35 @@ module Lita
       route(/[^\$]\$\(/, command: true, substitution: true) do |response|
         command, substitutions = parse(response.message.body)
 
+        substitutions.each do |position, subcommand|
+          channel = Channel.new(robot, subcommand, response.message.source)
+          channel.command!
+
+          result = ''
+          channel.on_reply do |*strings|
+            result << strings.flatten.join("\n")
+          end
+
+          robot.receive(channel)
+
+          command.insert(position, result)
+        end
+
         response.reply(command.inspect)
         response.reply(substitutions.inspect)
       end
 
     protected
+
+      class Channel < Message
+        def reply(*strings)
+          @on_reply.call(*strings) if @on_reply
+        end
+
+        def on_reply(&block)
+          @on_reply = block
+        end
+      end
 
       def parse(text)
         ['randcase ', { 9 => 'fake lorem sentence' }]
